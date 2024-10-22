4c29f7ffcc8fc9eadc9c1d9a6e327fc673480638
Emil Folino <emilfolino@gmail.com>
2024-10-22 13:41:46 +0200

    Fetches all forks and find all open pulls

--- app.mjs
+++ app.mjs
@@ -1,3 +1,34 @@
 import 'dotenv/config'
+import { Octokit } from "@octokit/core"
 
-console.log(process.env.GITHUB_API_TOKEN)
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_API_TOKEN
+})
+
+const response = await octokit.request('GET /repos/{owner}/{repo}/forks', {
+  owner: 'emilfolino',
+  repo: 'pr_tester',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+
+const data = response.data;
+
+for (let i = 0; i < data.length; i++) {
+    let fork = data[i];
+
+    const pulls = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+        owner: fork.owner.login,
+        repo: fork.name,
+        state: "open",
+        sort: "created",
+        direction: "desc",
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+
+    console.log(pulls.data)
+}
